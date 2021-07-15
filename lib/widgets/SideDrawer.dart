1a5c383031a7f70d445ff656f9ab4b4e4b462db9
surya0180 <teja8551@gmail.com>
2021-07-15 18:28:17 +0530

    Added bottom nav, side drawer and splash screen

---
+++ lib/widgets/SideDrawer.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SideDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text('Pharmassist'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.feedback),
+            title: Text('Feedback'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text('About'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed('/');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

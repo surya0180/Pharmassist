c2ffe21be6867a9edb8557a430f99793b437d89f
bannu0snake <mahaboob.developer@gmail.com>
2021-08-04 12:30:07 +0530

    added some

--- lib/screens/admin_requests_screen.dart
+++ lib/screens/admin_requests_screen.dart
@@ -1,12 +1,85 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AdminRequestScreen extends StatelessWidget {
-  const AdminRequestScreen({ Key key }) : super(key: key);
+class AdminRequestScreen extends StatefulWidget {
+  @override
+  _AdminRequestScreenState createState() => _AdminRequestScreenState();
+}
+
+class _AdminRequestScreenState extends State<AdminRequestScreen> {
+  int _value = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('pharmacist requests')
+                .snapshots(),
+            builder: (ctx, pharmSnapShot) {
+              if (pharmSnapShot.connectionState == ConnectionState.waiting) {
+                print("line 199");
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              print("line 24");
+              return StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection('medical requests')
+                      .snapshots(),
+                  builder: (ctx, storeSnapShot) {
+                    if (storeSnapShot.connectionState ==
+                        ConnectionState.waiting) {
+                      print("line 29");
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    print("line 34");
+                    final pharmReqs = pharmSnapShot.data.docs;
+                    final medicReqs = storeSnapShot.data.docs;
+                    return Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(20.0),
+                          child: Container(
+                            padding: EdgeInsets.only(left: 5),
+                            color: Colors.cyan,
+                            child: DropdownButton(
+                                value: _value,
+                                items: [
+                                  DropdownMenuItem(
+                                    child: Text("pharmacist requests"),
+                                    value: 1,
+                                  ),
+                                  DropdownMenuItem(
+                                    child: Text("medical requests"),
+                                    value: 2,
+                                  ),
+                                ],
+                                onChanged: (value) {
+                                  setState(() {
+                                    _value = value;
+                                  });
+                                }),
+                          ),
+                        ),
+                        (_value == 1)
+                            ? ListView.builder(
+                                itemCount: pharmReqs.length,
+                                itemBuilder: (ctx, i) {},
+                              )
+                            : ListView.builder(
+                                itemCount: medicReqs.length,
+                                itemBuilder: (ctx, i) {},
+                              )
+                      ],
+                    );
+                  });
+            }),
+      ),
     );
   }
 }
--- lib/screens/request_screen.dart
+++ lib/screens/request_screen.dart
@@ -13,29 +13,25 @@ class RequestScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _isAdmin =
-        Provider.of<UserProvider>(context, listen: false).getIsAdminStatus;
     return Container(
-      child: _isAdmin
-          ? RequestsWidget()
-          : ListView(
-              children: [
-                RequestCard(
-                  title: 'Medical',
-                  description:
-                      'Send a request to admin if you want to join a medical shop as pharmacist',
-                  cardColor: Colors.cyanAccent[100],
-                  route: MedicalRequestForm.routeName,
-                ),
-                RequestCard(
-                  title: 'Pharmacist',
-                  description:
-                      'Send a request to admin if you are searching for some pharmacists',
-                  cardColor: Colors.lightGreenAccent[100],
-                  route: PharmacistRequestForm.routeName,
-                ),
-              ],
-            ),
+      child: ListView(
+        children: [
+          RequestCard(
+            title: 'Medical',
+            description:
+                'Send a request to admin if you want to join a medical shop as pharmacist',
+            cardColor: Colors.cyanAccent[100],
+            route: MedicalRequestForm.routeName,
+          ),
+          RequestCard(
+            title: 'Pharmacist',
+            description:
+                'Send a request to admin if you are searching for some pharmacists',
+            cardColor: Colors.lightGreenAccent[100],
+            route: PharmacistRequestForm.routeName,
+          ),
+        ],
+      ),
     );
   }
 }
